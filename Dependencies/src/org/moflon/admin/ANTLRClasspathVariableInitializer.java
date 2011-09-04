df793b107c4e17f91cad62d43d761c54cfa74e8c
aanjorin <anthonyanjorin@users.noreply.github.com>
2011-09-04 08:36:14 +0000

    Consolidated classpath variable initializers, added src path attachments.

--- Dependencies/src/org/moflon/admin/ANTLRClasspathVariableInitializer.java
+++ Dependencies/src/org/moflon/admin/ANTLRClasspathVariableInitializer.java
@@ -2,35 +2,14 @@ package org.moflon.admin;
 
 import java.net.URL;
 
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jdt.core.ClasspathVariableInitializer;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
 import org.moflon.Activator;
 
-public class ANTLRClasspathVariableInitializer extends ClasspathVariableInitializer
+public class ANTLRClasspathVariableInitializer extends BasicClasspathVariableInitializer
 {
-
    @Override
-   public void initialize(String variable)
+   protected URL getURLToVariable()
    {
-      URL urlPath = Activator.getDefault().getPathRelToPlugIn("/lib/antlr-3.3-complete.jar", Activator.PLUGIN_ID);
-
-      String path = urlPath.getPath();
-      
-      if ("jar".equals(urlPath.getProtocol()))
-      {
-         // Normalize string representation of URL: i.e. extract path from "jar:<path>!"
-         path = path.substring(path.indexOf(':') + 1, path.indexOf('!'));
-      }
-      
-      try
-      {
-         JavaCore.setClasspathVariable(variable, new Path(path), null);
-      } catch (JavaModelException e)
-      {
-         e.printStackTrace();
-      }
+      return Activator.getDefault().getPathRelToPlugIn("/lib/antlr-3.3-complete.jar", Activator.PLUGIN_ID);
    }
 
 }
--- Dependencies/src/org/moflon/admin/DependenciesClasspathVariableInitializer.java
+++ Dependencies/src/org/moflon/admin/DependenciesClasspathVariableInitializer.java
@@ -9,33 +9,12 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.moflon.Activator;
 
-public class DependenciesClasspathVariableInitializer extends ClasspathVariableInitializer
+public class DependenciesClasspathVariableInitializer extends BasicClasspathVariableInitializer
 {
-
    @Override
-   public void initialize(String variable)
+   protected URL getURLToVariable()
    {
-      URL urlPath = Activator.getDefault().getPathRelToPlugIn("/", Activator.PLUGIN_ID);
-
-      String path = urlPath.getPath();
-
-      if ("jar".equals(urlPath.getProtocol()))
-      {
-         // Normalize string representation of URL: i.e. extract path from "jar:<path>!"
-         path = path.substring(path.indexOf(':') + 1, path.indexOf('!'));
-      } else
-      {
-         // Eclipse has been spawned from a host eclipse so add /bin
-         path = path + File.separator + "bin";
-      }
-
-      try
-      {
-         JavaCore.setClasspathVariable(variable, new Path(path), null);
-      } catch (JavaModelException e)
-      {
-         e.printStackTrace();
-      }
+     return Activator.getDefault().getPathRelToPlugIn("/", Activator.PLUGIN_ID);
    }
 
 }
