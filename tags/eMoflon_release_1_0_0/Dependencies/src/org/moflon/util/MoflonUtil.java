09aaa280eebf7578ff4ea68a9c1d1bb8261396e0
aanjorin <aanjorin@6d408d5e-1b43-482e-a039-8a6d498cf1c8>
2012-06-13 13:32:40 +0000

    eMoflon_release_1_0_0

---
+++ tags/eMoflon_release_1_0_0/Dependencies/src/org/moflon/util/MoflonUtil.java
@@ -0,0 +1,120 @@
+package org.moflon.util;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.DailyRollingFileAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * A collection of useful helper methods.
+ * 
+ * @author anjorin
+ * @author (last editor) $Author$
+ * @version $Revision$ $Date$
+ */
+public class MoflonUtil
+{
+   /**
+    * Set up logging globally
+    * 
+    * @param configFile
+    *           URL to log4j property configuration file
+    * @param logFile
+    *           Absolute path to log file
+    * @return false if unable to setup and configure logging
+    */
+   public static boolean configureLogging(URL configFile, String logFile)
+   {
+      try
+      {
+         Logger root = Logger.getRootLogger();
+         String configurationStatus = "";
+         if (configFile != null)
+         {
+            // Configure system using config
+            PropertyConfigurator.configure(configFile);
+            configurationStatus = "Log4j successfully configured using " + configFile;
+         } else
+         {
+            configurationStatus = "Set up logging without config file!";
+         }
+
+         // Set format and scheme for output in logfile
+         PatternLayout layout = new PatternLayout("%d %5p [%c{2}::%L] - %m%n");
+         DailyRollingFileAppender fileAppender;
+         fileAppender = new DailyRollingFileAppender(layout, logFile, "'.'yyyy-MM-dd");
+         root.addAppender(fileAppender);
+
+         // Indicate success
+         root.info(configurationStatus);
+         root.info("Logging to: " + logFile + "\n\n");
+         return true;
+      } catch (Exception e)
+      {
+         e.printStackTrace();
+         return false;
+      }
+   }
+
+   /**
+    * Copies contents of directory named at sourceDir to directory at destination
+    * 
+    * @param sourceDir
+    *           Directory to be copied. Can be located in a jar or in the local filesystem
+    * @param destination
+    *           Directory to be created
+    * @param filter
+    *           Specifies what file/subdirectories are to be ignored
+    * @throws IOException
+    */
+   public static void copyDirToDir(URL sourceDir, File destination, FileFilter filter) throws IOException
+   {
+      if ("file".equals(sourceDir.getProtocol()))
+      {
+         // Copy from filesystem
+         File dir = new File(sourceDir.getFile());
+         FileUtils.copyDirectory(dir, destination, filter);
+      } else if ("jar".equals(sourceDir.getProtocol()))
+      {
+         // Copy from jar
+         JarURLConnection conn = (JarURLConnection) sourceDir.openConnection();
+         JarFile jar = conn.getJarFile();
+
+         destination.mkdir();
+         Enumeration<? extends JarEntry> entries = jar.entries();
+         while (entries.hasMoreElements())
+         {
+            JarEntry entry = entries.nextElement();
+
+            if (entry.getName().startsWith(conn.getEntryName()) && 
+                  !entry.getName().equals(conn.getEntryName()))
+            {
+               int beginIndex = entry.getName().indexOf(conn.getEntryName()) + conn.getEntryName().length();
+               String childPath = entry.getName().substring(beginIndex);
+               
+               if (entry.isDirectory())
+               {
+                  // Handle directory
+                  File dir = new File(destination, childPath);
+                  if (filter.accept(dir))
+                     dir.mkdir();
+               } else
+               {
+                  // Handle file
+                  FileUtils.copyInputStreamToFile(jar.getInputStream(entry), new File(destination, childPath));
+               }
+            }
+         }
+      }
+   }
+}
---
+++ tags/eMoflon_release_1_0_0/Dependencies/src/org/moflon/util/eMoflonEMFUtil.java
@@ -0,0 +1,339 @@
+package org.moflon.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EStructuralFeature.Setting;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EContentsEList;
+import org.eclipse.emf.ecore.util.ECrossReferenceAdapter;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+public class eMoflonEMFUtil
+{
+   private static final Logger logger = Logger.getLogger(eMoflonEMFUtil.class);
+   
+   private static Map<EClassifier, String> clazzNames = new HashMap<EClassifier, String>();
+   
+   
+   /**
+    * Simple utility method to be used for testing. Loads a model from path.
+    * 
+    * @param ePackage
+    *           Package of metamodel (for initialization)
+    * @param path
+    *           Local file path to model to be loaded
+    * @param resourceSet
+    *           Can be null if resources are not connected
+    * 
+    * @return Loaded model
+    */
+  static public EObject loadModel(EPackage ePackage, String path, ResourceSet resourceSet)
+   {
+      initEMF(ePackage);
+
+      return loadModel(createFileURI(path, true), resourceSet);
+   }
+   
+   public static EObject loadModel(URI uri, ResourceSet resourceSet)
+   {
+      // Obtain a new resource set if necessary
+      if (resourceSet == null)
+         resourceSet = new ResourceSetImpl();
+
+      // Get the resource
+      Resource resource = resourceSet.getResource(uri, true);
+
+      // Add adapter for reverse navigation along unidirectional links
+      ECrossReferenceAdapter adapter = ECrossReferenceAdapter.getCrossReferenceAdapter(resourceSet);
+      if(adapter == null)
+         resourceSet.eAdapters().add(new ECrossReferenceAdapter());
+      
+      // Return root model element
+      return resource.getContents().get(0);
+   }
+
+  /**
+    * Simple utility method for testing, saves model to file.
+    * 
+    * @param ePackage
+    *           Package of metamodel (for initialization)
+    * @param root
+    *           Model to be saved
+    * @param path
+    *           Local destination for xmi file
+    * @return
+    */
+   static public boolean saveModel(EPackage ePackage, EObject root, String path)
+   {
+      initEMF(ePackage);
+
+      return saveModel(root, createFileURI(path, false));
+   }
+   
+   static public boolean saveModel(EObject root, URI path){
+	  if(root == null)
+		  throw new IllegalArgumentException("The model to be saved cannot be null");
+	   
+      // Obtain a new resource set
+      ResourceSet resourceSet = new ResourceSetImpl();
+
+      // Create a resource and add model
+      Resource resource = resourceSet.createResource(path);
+      resource.getContents().add(root);
+
+      // Save model to file
+      try
+      {
+         resource.save(null);
+         return true;
+      } catch (IOException e)
+      {
+         logger.error("Unable to save model to " + path + ". Error:" + e.getMessage());
+         e.printStackTrace();
+         return false;
+      }
+
+   }
+
+  
+   static public URI createFileURI(String path, boolean mustExist)
+   {
+      File filePath = new File(path);
+      if (!filePath.exists() && mustExist)
+         throw new IllegalArgumentException(path + " does not exist.");
+
+      return URI.createFileURI(filePath.getAbsolutePath());
+   }
+
+   static private void initEMF(EPackage ePackage)
+   {
+      // Initialize the model
+      logger.debug("Initializing " + ePackage.getName());
+
+      registerXMIFactoryAsDefault();
+   }
+
+   public static void registerXMIFactoryAsDefault()
+   {
+      // Add XMI factory to registry
+      Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+      Map<String, Object> m = reg.getExtensionToFactoryMap();
+      m.put("*", new XMIResourceFactoryImpl());
+   }
+
+   /**
+    * This method only works when you registered an appropriate adapter right after loading your model! Further
+    * documentation can be found here: http://sdqweb.ipd.kit.edu/wiki/EMF_Reverse_Lookup_/
+    * _navigating_unidirectional_references_bidirectional
+    * 
+    * @param target
+    *           the target of this reference
+    * @param sourceType
+    *           the type of the opposite objects you are looking for
+    * @return a collection of all opposite objects
+    */
+   public static Collection<?> getOppositeReference(EObject target, @SuppressWarnings("rawtypes") Class sourceType, String targetRoleName)
+   {
+      ECrossReferenceAdapter adapter = getCRAdapter(target);
+
+      Collection<EObject> returnList = new ArrayList<EObject>();
+
+      Collection<Setting> settings = adapter.getInverseReferences(target, true);
+      for (Setting setting : settings)
+      {
+         if (setting.getEStructuralFeature().getName().equals(targetRoleName))
+         {
+            EClassifier clazz = setting.getEObject().eClass();
+            String clazzName = getClazzNameWithPackagePrefix(clazz);
+
+            if (clazzName.equals(sourceType.getName()) || checkInheritance(sourceType, clazz))
+               returnList.add(setting.getEObject());
+         }
+      }
+
+      return returnList;
+   }
+
+	public static String getClazzNameWithPackagePrefix(EClassifier clazz) {
+		String clazzName = clazzNames.get(clazz);
+
+		if (clazzName == null) {
+			clazzName = clazz.getInstanceClass().getPackage().getName() + "." + clazz.getName();
+			clazzNames.put(clazz, clazzName);
+		}
+		return clazzName;
+	}
+
+	public static boolean checkInheritance(Class superclass, EClassifier subclass) {
+		for (EClass sup : ((EClass)subclass).getEAllSuperTypes()) {
+			String clazzName = getClazzNameWithPackagePrefix(sup);
+			if(clazzName.equals(superclass.getName()))
+				return true;
+		}
+		return false;		
+	}
+
+   private static ECrossReferenceAdapter getCRAdapter(EObject target)
+   {
+      // Determine context
+      Notifier context = null;
+      
+      EObject root = EcoreUtil.getRootContainer(target, true);
+      Resource resource = root.eResource();
+      
+      if (resource != null)
+      {
+         ResourceSet resourceSet = resource.getResourceSet();
+         if(resourceSet != null)
+            context = resourceSet;
+         else
+            context = resource;
+      }else
+         context = root;
+      
+      // Retrieve adapter and create+add on demand
+      ECrossReferenceAdapter adapter = ECrossReferenceAdapter.getCrossReferenceAdapter(context);
+      if(adapter == null){
+         adapter = new ECrossReferenceAdapter();
+         context.eAdapters().add(adapter);
+      }
+      
+      return adapter;
+   }
+
+   public static void remove(EObject object)
+   {
+      EcoreUtil.delete(object, true);
+   }
+
+   @SuppressWarnings({ "unchecked", "rawtypes" })
+   public static void addOppositeReference(EObject source, EObject target, String targetRole)
+   {
+      EStructuralFeature reference = source.eClass().getEStructuralFeature(targetRole);
+      if (!reference.isMany())
+      {
+         source.eSet(reference, target);
+      } else
+         ((Collection) source.eGet(reference)).add(target);
+   }
+
+   @SuppressWarnings({ "rawtypes" })
+   public static void removeOppositeReference(EObject source, EObject target, String targetRole)
+   {
+      EStructuralFeature reference = source.eClass().getEStructuralFeature(targetRole);
+      if (!reference.isMany())
+      {
+         source.eSet(reference, null);
+      } else
+         ((Collection) source.eGet(reference)).remove(target);
+
+   }
+   
+   public static EStructuralFeature getReference(EObject obj, String name){
+      for (EContentsEList.FeatureIterator featureIterator = (EContentsEList.FeatureIterator) obj.eCrossReferences().iterator(); featureIterator.hasNext();)
+      {
+         featureIterator.next();
+         EReference eReference = (EReference) featureIterator.feature();
+         if (eReference.getName().equals(name))
+            return eReference;
+      }
+
+      return null;
+   }
+   
+   public static EStructuralFeature getContainment(EObject container, String name){
+      for (EContentsEList.FeatureIterator featureIterator = (EContentsEList.FeatureIterator) container.eContents().iterator(); featureIterator.hasNext();)
+      {
+         featureIterator.next();
+         EReference eReference = (EReference) featureIterator.feature();
+         if (eReference.getName().equals(name))
+            return eReference;
+      }
+
+      return null;
+   }
+   
+   /**
+    * Calculates the set of all outgoing references of a given EObject. 
+    * @param object
+    * @return set of references
+    */
+   public static Set<EStructuralFeature> getAllReferences(EObject object){
+	   EList<EStructuralFeature> references = new BasicEList<EStructuralFeature>();
+	   for (EContentsEList.FeatureIterator featureIterator = (EContentsEList.FeatureIterator) object.eCrossReferences().iterator(); featureIterator.hasNext();)
+	   {
+		   featureIterator.next();
+		   references.add(featureIterator.feature());
+	   }
+	   for (EContentsEList.FeatureIterator featureIterator = (EContentsEList.FeatureIterator) object.eContents().iterator(); featureIterator.hasNext();)
+	   {
+		   featureIterator.next();
+		   references.add(featureIterator.feature());
+	   }
+	   return new HashSet<EStructuralFeature>(references);
+   }
+
+	public static String getName(EObject child) {		
+		Object name = "";
+		
+		EStructuralFeature nameFeature = (EStructuralFeature) child.eClass().getEStructuralFeature("name");		
+				
+		if(nameFeature != null)
+			name = child.eGet(nameFeature);
+		
+		return (String) (name instanceof String && !((String) name).equals("") ? name : child.toString());
+	}
+	
+	public static void addToResourceSet(ResourceSet set, EObject object) {
+		Resource resource = new ResourceImpl();
+		resource.setURI(URI.createURI(object.eClass().getEPackage().getNsURI()));
+		resource.getContents().add(object);
+		set.getResources().add(resource);
+	}
+	
+   public static void embedSDMInEAnnotation(EObject sdm, EAnnotation eAnnotation)
+   {
+      ResourceSet resourceSet = new ResourceSetImpl();
+      Resource resource = resourceSet.createResource(URI.createURI(""));
+      resource.getContents().add(sdm);
+
+      StringWriter writer = new StringWriter();
+      OutputStream out = new URIConverter.WriteableOutputStream(writer, "UTF-8");
+      try
+      {
+         resource.save(out, null);
+      } catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+
+      eAnnotation.getDetails().put("XMI", writer.toString());
+   }
+
+}
