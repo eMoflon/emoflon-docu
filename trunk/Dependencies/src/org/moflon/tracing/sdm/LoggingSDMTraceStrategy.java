5179c5a4b786c4e3304b04f6120b836bad31b66e
mwieber <mwieber@6d408d5e-1b43-482e-a039-8a6d498cf1c8>
2012-08-24 09:13:48 +0000

    provided basic implementation for LoggingSDMTraceStrategy

--- trunk/Dependencies/src/org/moflon/tracing/sdm/LoggingSDMTraceStrategy.java
+++ trunk/Dependencies/src/org/moflon/tracing/sdm/LoggingSDMTraceStrategy.java
@@ -1,65 +1,68 @@
 package org.moflon.tracing.sdm;
 
+import org.apache.log4j.Logger;
 import org.eclipse.emf.ecore.EOperation;
+import org.moflon.tracing.sdm.events.BindObjectVarEvent;
+import org.moflon.tracing.sdm.events.MatchFoundEvent;
+import org.moflon.tracing.sdm.events.NoMatchFoundEvent;
+import org.moflon.tracing.sdm.events.OperationEnterEvent;
+import org.moflon.tracing.sdm.events.OperationExitEvent;
+import org.moflon.tracing.sdm.events.PatternEnterEvent;
+import org.moflon.tracing.sdm.events.PatternExitEvent;
+import org.moflon.tracing.sdm.events.UnbindObjectVarEvent;
 
 public class LoggingSDMTraceStrategy extends SDMTraceStrategy {
 
+	Logger log = Logger.getLogger(LoggingSDMTraceStrategy.class);
+	
 	@Override
 	protected void logOperationEnter(SDMTraceContext c, StackTraceElement ste,
 			EOperation op, Object[] parameterValues) {
-		// TODO Auto-generated method stub
-
+		log.debug((new OperationEnterEvent(op, parameterValues)).toString());
 	}
 
 	@Override
 	protected void logOperationExit(SDMTraceContext c, StackTraceElement ste,
 			EOperation op, Object result) {
-		// TODO Auto-generated method stub
-
+		log.debug((new OperationExitEvent(op, result)).toString());
 	}
 
 	@Override
 	protected void logPatternEnter(SDMTraceContext c, StackTraceElement ste,
 			String storyPatternName, EOperation op) {
-		// TODO Auto-generated method stub
-
+		log.debug((new PatternEnterEvent(storyPatternName, op)).toString());
 	}
 
 	@Override
 	protected void logPatternExit(SDMTraceContext c, StackTraceElement ste,
 			String storyPatternName, EOperation op) {
-		// TODO Auto-generated method stub
-
+		log.debug((new PatternExitEvent(storyPatternName, op)).toString());
 	}
 
 	@Override
 	protected void logBindObjVar(SDMTraceContext c, StackTraceElement ste,
 			String objVarName, Class<?> objVarType, Object oldValue,
 			Object newValue) {
-		// TODO Auto-generated method stub
-		
+		log.debug((new BindObjectVarEvent(objVarName, objVarType, oldValue, newValue)).toString());
 	}
 
 	@Override
 	protected void logUnbindObjVar(SDMTraceContext c, StackTraceElement ste,
 			String objVarName, Class<?> objVarType, Object oldValue,
 			Object newValue) {
-		// TODO Auto-generated method stub
-		
+		log.debug((new UnbindObjectVarEvent(objVarName, objVarType, oldValue, newValue)).toString());		
 	}
 
 	@Override
 	protected void logMatchFound(SDMTraceContext c, StackTraceElement ste,
 			EOperation op, Object... paramValues) {
-		// TODO Auto-generated method stub
-		
+		log.debug((new MatchFoundEvent(op, paramValues)).toString());
 	}
 
 	@Override
 	protected void logNoMatchFound(SDMTraceContext c, StackTraceElement ste,
 			EOperation op, Object... paramValues) {
-		// TODO Auto-generated method stub
-		
+		log.debug((new NoMatchFoundEvent(op, paramValues)).toString());
 	}
 
 }
