6e484c56a1665feb1cb21d4452bd625a24a5f3f7
anjorin <anjorin@6d408d5e-1b43-482e-a039-8a6d498cf1c8>
2013-11-16 09:47:31 +0000

    Preparing for Kepler

--- trunk/Dependencies/src/org/moflon/util/EmfCompareUtil.java
+++ trunk/Dependencies/src/org/moflon/util/EmfCompareUtil.java
@@ -6,35 +6,60 @@ import java.util.List;
 import org.eclipse.emf.compare.Comparison;
 import org.eclipse.emf.compare.Diff;
 import org.eclipse.emf.compare.EMFCompare;
+import org.eclipse.emf.compare.Match;
+import org.eclipse.emf.compare.diff.DefaultDiffEngine;
+import org.eclipse.emf.compare.diff.DiffBuilder;
+import org.eclipse.emf.compare.diff.FeatureFilter;
+import org.eclipse.emf.compare.diff.IDiffEngine;
+import org.eclipse.emf.compare.diff.IDiffProcessor;
 import org.eclipse.emf.compare.scope.IComparisonScope;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcorePackage;
 
 /**
  * Collection of useful methods for EMFCompare
- *
+ * 
  * @author anjorin
  * @author (last editor) $Author$
  * @version $Revision$ $Date$
  */
 public class EmfCompareUtil
 {
-   	
+
    /**
     * Compares actual to expected and returns a list of differences, filtered according to the filter flags.
     * 
     * @param actual
     * @param expected
+    * @param ignoreReferenceOrder
     * 
     * @return <b>Unmodifiable</b> list of filtered differences.
     * 
     * @throws IOException
     * @throws InterruptedException
     */
-   public static List<Diff> compareAndFilter(EObject actual, EObject expected, boolean ignoreReferenceOrder) throws InterruptedException
+   public static List<Diff> compareAndFilter(EObject actual, EObject expected, final boolean ignoreReferenceOrder) throws InterruptedException
    {
-	   IComparisonScope scope = EMFCompare.createDefaultScope(actual, expected);
-	   Comparison comparison = EMFCompare.builder().build().compare(scope);
-	 
-	   return comparison.getDifferences();
+      IDiffProcessor diffProcessor = new DiffBuilder();
+      IDiffEngine diffEngine = new DefaultDiffEngine(diffProcessor) {
+         @Override
+         protected FeatureFilter createFeatureFilter()
+         {
+            return new FeatureFilter() {
+               @Override
+               public boolean checkForOrderingChanges(EStructuralFeature feature)
+               {
+                  return !ignoreReferenceOrder;
+               }
+            };
+         }
+      };
+
+      IComparisonScope scope = EMFCompare.createDefaultScope(actual, expected);
+      Comparison comparison = EMFCompare.builder().setDiffEngine(diffEngine).build().compare(scope);
+
+      return comparison.getDifferences();
    }
 }
