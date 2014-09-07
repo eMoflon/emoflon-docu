04e04221d877f571be1da3f0268264c4c85dd3a0
rkluge <rkluge@6d408d5e-1b43-482e-a039-8a6d498cf1c8>
2014-09-07 12:50:53 +0000

    Finally, managed to kill Eclipse instances programmatically

--- trunk/WorkspaceConfiguration/autoTestRunner/killTestEclipseInstances.ps1
+++ trunk/WorkspaceConfiguration/autoTestRunner/killTestEclipseInstances.ps1
@@ -1,9 +1,27 @@
-echo "Does not work yet, because Eclipse keeps running, even after stopping its process"
-exit
+function Find-ChildProcess {
+param($ID=$PID)
+
+$CustomColumnID = @{
+	Name = 'Id'
+	Expression = { [Int[]]$_.ProcessID }
+}
+
+$result = Get-WmiObject -Class Win32_Process -Filter "ParentProcessID=$ID" |
+Select-Object -Property ProcessName, $CustomColumnID, CommandLine
+
+$result
+$result | Where-Object { $_.ID -ne $null } | ForEach-Object {
+	Find-ChildProcess -id $_.Id
+}
+}
+
 
 $OUTPUT_DIRECTORY="$HOME\emoflon_autotest_tmp_dir"
 $filename = "$OUTPUT_DIRECTORY\pids.txt"
 foreach ($id in [System.IO.File]::ReadLines($filename)) {
-   echo "Stopping $id"
-   Stop-Process $id
+   #echo "Stopping $id"
+   #Stop-Process $id
+   
+   $child = Find-ChildProcess $id
+	(Get-Process -Id $child.Id).CloseMainWindow()
 }
