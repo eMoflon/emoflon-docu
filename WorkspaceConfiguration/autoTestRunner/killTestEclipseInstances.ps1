b862eb21746a2c2e32c291bc5d91276338a8553e
Roland K <RolandKluge@users.noreply.github.com>
2014-09-08 15:55:56 +0000

    Format script

--- WorkspaceConfiguration/autoTestRunner/killTestEclipseInstances.ps1
+++ WorkspaceConfiguration/autoTestRunner/killTestEclipseInstances.ps1
@@ -1,27 +1,25 @@
+# http://powershell.com/cs/blogs/tobias/archive/2012/05/09/managing-child-processes.aspx
 function Find-ChildProcess {
-param($ID=$PID)
+	param($ID=$PID)
 
-$CustomColumnID = @{
-	Name = 'Id'
-	Expression = { [Int[]]$_.ProcessID }
+	$CustomColumnID = @{
+		Name = 'Id'
+		Expression = { [Int[]]$_.ProcessID }
+	}
+	
+	$result = Get-WmiObject -Class Win32_Process -Filter "ParentProcessID=$ID" |
+	Select-Object -Property ProcessName, $CustomColumnID, CommandLine
+	
+	$result
+	$result | Where-Object { $_.ID -ne $null } | ForEach-Object {
+		Find-ChildProcess -id $_.Id
+	}
 }
 
-$result = Get-WmiObject -Class Win32_Process -Filter "ParentProcessID=$ID" |
-Select-Object -Property ProcessName, $CustomColumnID, CommandLine
 
-$result
-$result | Where-Object { $_.ID -ne $null } | ForEach-Object {
-	Find-ChildProcess -id $_.Id
-}
-}
-
-
-$OUTPUT_DIRECTORY="$HOME\emoflon_autotest_tmp_dir"
-$filename = "$OUTPUT_DIRECTORY\pids.txt"
+$outputDirectory="$HOME\emoflon_autotest_tmp_dir"
+$filename = "$outputDirectory\pids.txt"
 foreach ($id in [System.IO.File]::ReadLines($filename)) {
-   #echo "Stopping $id"
-   #Stop-Process $id
-   
    $child = Find-ChildProcess $id
 	(Get-Process -Id $child.Id).CloseMainWindow()
 }
