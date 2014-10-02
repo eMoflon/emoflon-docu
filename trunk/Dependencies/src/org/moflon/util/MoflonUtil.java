24b9733f1f96965301e049db4a37962c693bf47a
rkluge <rkluge@6d408d5e-1b43-482e-a039-8a6d498cf1c8>
2014-10-02 12:12:41 +0000

    Create default method body for unimplemented methods in Democles

--- trunk/Dependencies/src/org/moflon/util/MoflonUtil.java
+++ trunk/Dependencies/src/org/moflon/util/MoflonUtil.java
@@ -19,14 +19,25 @@ import org.eclipse.emf.ecore.EcorePackage;
 /**
  * A collection of useful helper methods.
  * 
- * @author anjorin
- * @author (last editor) $Author$
- * @version $Revision$ $Date$
  */
 public class MoflonUtil
 {
-   
-   public static String getMoflonDefaultURIForProject(final String projectName){
+
+   /**
+    * Marker for code passages generated through eMoflon/EMF that are eligible for 
+    * extracting injections.
+    */
+   public static final String EOPERATION_MODEL_COMMENT = "// [user code injected with eMoflon]";
+
+   /**
+    * Code corresponding to the default implementation of a java method. Is used, when no SDM implementation could be
+    * retrieved.
+    */
+   public final static String DEFAULT_METHOD_BODY = "\n" + EOPERATION_MODEL_COMMENT
+         + "\n\n// TODO: implement this method here but do not remove the injection marker \nthrow new UnsupportedOperationException();";
+
+   public static String getMoflonDefaultURIForProject(final String projectName)
+   {
       return "http://www.moflon.org." + projectName;
    }
 
@@ -60,12 +71,11 @@ public class MoflonUtil
          {
             JarEntry entry = entries.nextElement();
 
-            if (entry.getName().startsWith(conn.getEntryName()) && 
-                  !entry.getName().equals(conn.getEntryName()))
+            if (entry.getName().startsWith(conn.getEntryName()) && !entry.getName().equals(conn.getEntryName()))
             {
                int beginIndex = entry.getName().indexOf(conn.getEntryName()) + conn.getEntryName().length();
                String childPath = entry.getName().substring(beginIndex);
-               
+
                if (entry.isDirectory())
                {
                   // Handle directory
@@ -81,74 +91,79 @@ public class MoflonUtil
          }
       }
    }
-   
+
    /**
-	 * Derive the java data type of a given Ecore data type. 
-	 * All numerical data types (e.g. byte, int etc.) are converted to the Java class "Number".
-	 * 
-	 * @param eCoreType the name of the Ecore data type class (e.g. EString)
-	 * @return the name of the java type class (e.g. String)
-	 */
-	public static String eCoreTypeToJavaType(final String eCoreType, final boolean numericalToNumber) throws IllegalArgumentException 
-	{
-		String javaType = "";
-		List<String> primitiveNumberTypes = Arrays.asList(new String[] {
-				"byte", "short", "int", "long", "float", "double" });
-	   
-		// Derive the java data type from the Ecore class name 
-		try {
-			javaType = EcorePackage.eINSTANCE.getEClassifier(eCoreType).getInstanceClass().getSimpleName();
-		} catch (Exception e) {
-			System.err.println("Can not derive java data type from the given Ecore data type = " + eCoreType);
-			
-			javaType = eCoreType;
-		}
-	 
-		// Convert all numerical data types to Number
-		if(numericalToNumber)
-			if (primitiveNumberTypes.contains(javaType))
-				javaType = "Number";
-		   
-		return javaType;
-	}
-	
-	/**
-	 * Determine fully qualified name of given element by iterating through package hierarchy.
-	 * @param ENamedElement
-	 * @return
-	 */
-	public static String getFQN(final ENamedElement element) {
-		String fqn = element.getName();
+    * Derive the java data type of a given Ecore data type. All numerical data types (e.g. byte, int etc.) are converted
+    * to the Java class "Number".
+    * 
+    * @param eCoreType
+    *           the name of the Ecore data type class (e.g. EString)
+    * @return the name of the java type class (e.g. String)
+    */
+   public static String eCoreTypeToJavaType(final String eCoreType, final boolean numericalToNumber) throws IllegalArgumentException
+   {
+      String javaType = "";
+      List<String> primitiveNumberTypes = Arrays.asList(new String[] { "byte", "short", "int", "long", "float", "double" });
 
-		ENamedElement e = element;
+      // Derive the java data type from the Ecore class name
+      try
+      {
+         javaType = EcorePackage.eINSTANCE.getEClassifier(eCoreType).getInstanceClass().getSimpleName();
+      } catch (Exception e)
+      {
+         System.err.println("Can not derive java data type from the given Ecore data type = " + eCoreType);
 
-		while (e.eContainer() != null) {
-			e = (ENamedElement) e.eContainer();
-			fqn = e.getName() + "." + fqn;
-		}
+         javaType = eCoreType;
+      }
 
-		return fqn;
-	}
+      // Convert all numerical data types to Number
+      if (numericalToNumber)
+         if (primitiveNumberTypes.contains(javaType))
+            javaType = "Number";
 
-	/**
-	 * This method realizes our convention for determining the package name of a given EPackage.name
-	 * In the future this could be replaced by using the genmodel.
-	 * 
-	 * @param packageName
-	 * @return
-	 */
+      return javaType;
+   }
+
+   /**
+    * Determine fully qualified name of given element by iterating through package hierarchy.
+    * 
+    * @param ENamedElement
+    * @return
+    */
+   public static String getFQN(final ENamedElement element)
+   {
+      String fqn = element.getName();
+
+      ENamedElement e = element;
+
+      while (e.eContainer() != null)
+      {
+         e = (ENamedElement) e.eContainer();
+         fqn = e.getName() + "." + fqn;
+      }
+
+      return fqn;
+   }
+
+   /**
+    * This method realizes our convention for determining the package name of a given EPackage.name In the future this
+    * could be replaced by using the genmodel.
+    * 
+    * @param packageName
+    * @return
+    */
    public static String determinePackageName(final String packageName)
    {
       switch (packageName)
       {
       case "uml":
          return "UMLPackage";
-         
+
       default:
          return packageName.substring(0, 1).toUpperCase() + packageName.substring(1) + "Package";
-      }  
+      }
    }
-   
+
    public static boolean toBePluralized(final String roleName, final String packageName, final String typeName, final boolean toOne)
    {
       switch (packageName)
@@ -156,7 +171,7 @@ public class MoflonUtil
       case "uml":
          // For UML, toMany links are pluralized!
          return !toOne;
-         
+
       default:
          return false;
       }
@@ -166,13 +181,13 @@ public class MoflonUtil
    {
       final String is = "is";
       final String prefix = ".is" + StringUtils.capitalize(attribute);
-      
+
       switch (packageName)
       {
       case "uml":
          // For UML only return prefix if the attribute does not already start with an "is"
-         return attribute.startsWith(is)? "." + attribute : prefix;
-         
+         return attribute.startsWith(is) ? "." + attribute : prefix;
+
       default:
          return prefix;
       }
