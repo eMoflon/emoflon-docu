ef7ae459e01ae752c418b81cf5ba470eeebfc355
Roland K <RolandKluge@users.noreply.github.com>
2014-11-19 16:17:24 +0000

    Working on new metamodel builder
    (additionally: make one method deprecated)

---
+++ Dependencies/testsrc/org/moflon/util/MoflonUtilTest.java
@@ -0,0 +1,36 @@
+package org.moflon.util;
+
+import org.eclipse.emf.codegen.ecore.genmodel.GenModelFactory;
+import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoflonUtilTest
+{
+   @Test
+   public void testFQNForGenPackages() throws Exception
+   {
+      
+      GenPackage topPackage = createGenPackage("1");
+      GenPackage mediumPackage = createGenPackage("2");
+      GenPackage lowestPackage = createGenPackage("3");
+      Assert.assertEquals("1.2.3", MoflonUtil.getFQN(lowestPackage));
+   }
+
+   public GenPackage createGenPackage(final String label)
+   {
+      EPackage ePackage = createEPackage(label);
+      GenPackage genPackage = GenModelFactory.eINSTANCE.createGenPackage();
+      genPackage.setEcorePackage(ePackage);
+      return genPackage;
+   }
+
+   public EPackage createEPackage(final String name)
+   {
+      EPackage ePackage = EcoreFactory.eINSTANCE.createEPackage();
+      ePackage.setName(name);
+      return ePackage;
+   }
+}
