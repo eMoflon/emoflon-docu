7ecd5366f3ce5a16b1c7f6a489d28f0ac7a85501
Roland K <RolandKluge@users.noreply.github.com>
2015-05-20 14:58:28 +0000

    Update autotest runner

--- org.moflon.doc.WorkspaceConfiguration/autoTestRunner/eMoflonTestRunner.ps1
+++ org.moflon.doc.WorkspaceConfiguration/autoTestRunner/eMoflonTestRunner.ps1
@@ -1,22 +1,45 @@
-$OLD_DIR = $(get-location).Path
-$OUTPUT_DIRECTORY="$HOME\emoflon_autotest_tmp_dir"
+#$OLD_DIR = $(get-location).Path
 
-# List of all test workspaces
-$WORKSPACES = 
-	"TestWorkspace_Democles_0",
- 	"TestWorkspace_Misc",
-	"TestWorkspace_TGG_0",
-	"TestWorkspace_TGG_1",
-	"TransformationZoo_0",
-	"TransformationZoo_1",
-	"DeveloperWorkspace"
+# Directory for temporarily storing the checked out projects
+$OUTPUT_DIRECTORY="[set output directory]"
 
 # Path to eclipse - must contain eclipse.exe
-$ECLIPSE_HOME = "C:\Program Files\eclipse"
+$ECLIPSE_HOME = "[set Eclipse home]"
 
-echo "OUTPUT_DIRECTORY:   $OUTPUT_DIRECTORY"
-echo "ECLIPSE_HOME:       $ECLIPSE_HOME"
+# Time between two executions of Eclipse
+$sleepTimeBetweenStartsInSeconds = 120
+
+# Array of all test workspaces
+# (see EMoflonStandardWorkspaces)
+[System.Collections.ArrayList]$WORKSPACES = @()
+$WORKSPACES.add("TestWorkspace_Democles_0")
+$WORKSPACES.add("TestWorkspace_Misc")
+# $WORKSPACES.add("TestWorkspace_TGG_0")
+# $WORKSPACES.add("TestWorkspace_TGG_1")
+# $WORKSPACES.add("TransformationZoo_0")
+# $WORKSPACES.add("TransformationZoo_1")
+# $WORKSPACES.add("DeveloperWorkspace")
+# $WORKSPACES.add("TextualSyntaxWorkspace")
+
+# Whether to spawn a new console for each Eclipse instance, showing standard output/standard error messages
+$USE_CONSOLE = $FALSE # either $TRUE or $FALSE
+
+
+# Number of trials to clean the output directory
+$NUM_TRIALS_FOR_CLEANING_OUTPUT_DIRECTORY = 5
+
+echo "OUTPUT_DIRECTORY:         $OUTPUT_DIRECTORY"
+echo "ECLIPSE_HOME:             $ECLIPSE_HOME"
+echo "Use console?              $USE_CONSOLE"
+echo "Interval between starts:  $sleepTimeBetweenStartsInSeconds"
+echo "org.moflon.ide version:   $(Get-ChildItem -Name "$ECLIPSE_HOME/plugins/org.moflon.ide.core*")"
+echo "Workspaces to be run:     $WORKSPACES" 
 echo ""
+$confirmed = Read-Host "Continue? [Y/n]"
+if($confirmed -ne "" -and ($confirmed -ne "y" -or $confirmed -ne "Y")) {
+    echo "User aborted. Bye."
+    exit
+}
 
 # Clean up root folder
 echo "Cleaning output directory (possibly with multiple runs)..."
@@ -28,7 +51,7 @@ while(Test-Path $OUTPUT_DIRECTORY) {
 	
 	Remove-Item -Recurse -Force "$OUTPUT_DIRECTORY"
 	$i = $i + 1
-	if($i -gt 10) {
+	if($i -gt $NUM_TRIALS_FOR_CLEANING_OUTPUT_DIRECTORY) {
 		echo "Maximum number of trials reached. Please try to clean the output directory, manually."
 		echo "Will now stop."
 		exit
@@ -39,17 +62,24 @@ echo "Cleaning output directory done."
 
 echo "Creating output directory"
 mkdir $OUTPUT_DIRECTORY | out-null
-echo "Changing to output directory"
-cd $OUTPUT_DIRECTORY
-
 
 echo "Starting Eclipse instances..."
-# Start Eclipse for all workspaces			  
+# Start Eclipse for all workspaces
+$firstIteration = $TRUE
 foreach ($WORKSPACE in $WORKSPACES) {
-  	$eclipse = Start-Process -FilePath $ECLIPSE_HOME\eclipse.exe -ArgumentList '-data',$WORKSPACE,'-application','org.moflon.testapplication','-showLocation'   -PassThru
+    if (!($WORKSPACE -eq $WORKSPACES[0]) ) {
+        $firstIteration = $FALSE
+        echo "    Sleeping for $sleepTimeBetweenStartsInSeconds seconds..."
+        Start-Sleep -s $sleepTimeBetweenStartsInSeconds
+    }
+    
+    [System.Collections.ArrayList]$argumentList = '-data',$WORKSPACE,'-application','org.moflon.testapplication','-showLocation','-perspective','org.moflon.ide.ui.perspective'
+    if($USE_CONSOLE) {
+        $argumentList.Add('-console')
+        $argumentList.Add('-consoleLog')
+    }
+    
+  	$eclipse = Start-Process -WorkingDirectory $OUTPUT_DIRECTORY -FilePath $ECLIPSE_HOME\eclipse.exe -ArgumentList $argumentList   -PassThru
   	echo "    [$($eclipse.Id)] Workspace '$WORKSPACE'"
   	$eclipse.Id >> "$OUTPUT_DIRECTORY\pids.txt"
 }
-
-
-cd $OLD_DIR
